fbc16dfd5e1f257f463e372e7f0ee9d8cd944ef1
Dnpypy <denlatypoff@yandex.ru>
2021-10-06 17:19:55 +0300

    Initial commit

--- Topics/Conditional statement/Interval/src/Main.java
+++ Topics/Conditional statement/Interval/src/Main.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        final int a = -15;
+        final int b = 14;
+        final int c = 12;
+        final int d = 17;
+        final int e = 19;
+
+
+        if (n > a && n <= c) {
+            System.out.println("True");
+
+        } else if (b < n && n < d) {
+            System.out.println("True");
+
+        } else if (e <= n) {
+            System.out.println("True");
+
+        } else {
+            System.out.println("False");
+        }
+
+
     }
 }
