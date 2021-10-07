28ed1f5a2132bcedb024b204640f21c735352e30
Dnpypy <denlatypoff@yandex.ru>
2021-10-07 15:51:37 +0300

    Initial commit

--- Topics/Conditional statement/The army of units/src/Main.java
+++ Topics/Conditional statement/The army of units/src/Main.java
@@ -1,5 +1,28 @@
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        final int pack = 1;
+        final int packTi = 19;
+        final int throng = 20;
+        final int throngTi = 249;
+        final int zounds = 250;
+        final int zoundsTi = 999;
+
+        if (n < pack) {
+            System.out.println("no army");
+        } else if (pack <= n && n <= packTi) {
+            System.out.println("pack");
+        } else if (throng <= n && n <= throngTi) {
+            System.out.println("throng");
+        } else if (zounds <= n && n <= zoundsTi) {
+            System.out.println("zounds");
+        } else {
+            System.out.println("legion");
+        }
+
     }
 }
