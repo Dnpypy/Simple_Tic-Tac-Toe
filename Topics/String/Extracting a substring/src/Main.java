e31ef809d9d44ae61062715b70fc58a96213c378
Dnpypy <denlatypoff@yandex.ru>
2021-10-08 10:42:25 +0300

    Initial commit

--- Topics/String/Extracting a substring/src/Main.java
+++ Topics/String/Extracting a substring/src/Main.java
@@ -1,5 +1,14 @@
+
+import java.util.Scanner;
+
 class Main {
     public static void main(String[] args) {
         // put your code here
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine().trim();
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        String c = s.substring(a, b + 1);
+        System.out.println(c);
     }
 }
