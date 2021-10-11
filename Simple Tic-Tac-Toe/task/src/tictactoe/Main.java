92a15731eb12678b1d09a02c8ff529775607af4a
Dnpypy <denlatypoff@yandex.ru>
2021-10-11 17:14:01 +0300

    Initial commit

--- Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,39 +1,30 @@
 package tictactoe;
 
 import java.util.Scanner;
-//1. Turn input string toCharArray().
-//2. Create a 3*3 char array(), then fill it with a double for loop, you can fit that | in your for loop as well.
-//3. And don't forget to put that -------- before and after your for loops.
+
 public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner scanner = new Scanner(System.in);
-        // put your code here
         System.out.println("Enter cells:");
         String s = scanner.next().trim();
-        if(s.equals("O_OXXO_XX")) {
-            System.out.println("---------\n" +
-           "| O _ O |\n" +
-           "| X X O |\n" +
-           "| _ X X |\n" +
-           "---------");
-        } else if (s.equals("OXO__X_OX")) {
-            System.out.println("---------\n" +
-           "| O X O |\n" +
-           "| _ _ X |\n" +
-           "| _ O X |\n" +
-           "---------");
-        } else if (s.equals("_XO__X___")) {
-            System.out.println("---------\n" +
-            "| _ X O |\n" +
-            "| _ _ X |\n" +
-            "| _ _ _ |\n" +
-            "---------");
-        }
-        if (s.equals("_________")) {
+        s = s.replace("", " ");
+
+        final String line = "---------";
+        char[] aChr = s.toCharArray();
+
+        System.out.println(line);
+        for(int i = 0; i < aChr.length; i++) {
+            if(i == 6 || i == 12 || i == 18) {
+                System.out.print(" |");
+                System.out.println();
+            }
+            if(i == 0 || i == 6 || i == 12) {
+                System.out.print("|");
+            }
+            System.out.print(i < aChr.length - 1 ? aChr[i] : "");
 
         }
-
-
+        System.out.print(line);
     }
 }
