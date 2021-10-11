95a836c48d84204e5f7fd6a0a26d5d4bb3510464
Dnpypy <denlatypoff@yandex.ru>
2021-10-11 18:07:32 +0300

    Initial commit

---
+++ Topics/Array/Initializing an array of longs/src/Main.java
@@ -0,0 +1,15 @@
+import java.util.Arrays;
+
+public class Main {
+
+    private static final long AA = 100000000001L;
+    private static final long BB = 100000000002L;
+    private static final long CC = 100000000003L;
+
+    public static void main(String[] args) {
+
+        long[] longNumbers = {AA, BB, CC};
+
+        System.out.println(Arrays.toString(longNumbers));
+    }
+}
