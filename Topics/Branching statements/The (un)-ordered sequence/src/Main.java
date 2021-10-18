4f897bb70ae98200f78ab4a6f73ae125f62f33ab
Dnpypy <denlatypoff@yandex.ru>
2021-10-18 17:17:07 +0300

    Changes

--- Topics/Branching statements/The (un)-ordered sequence/src/Main.java
+++ Topics/Branching statements/The (un)-ordered sequence/src/Main.java
@@ -5,47 +5,35 @@ class Main {
     public static void main(String[] args) {
         // put your code here
         Scanner sc = new Scanner(System.in);
-        ArrayList<Integer> arr = new ArrayList<Integer>();
-        int one = 0;
-        int two = 0;
-        boolean tt = true;
-        int i = 0;
-        int count = 0;
-        int countTwo = 0;
-        int s = 0;
+        int neW = sc.nextInt();
+        int olD = neW;
+
+        int asc = 0;  // >>>>>>
+        int desc = 0; /// <<<<<<<<
+        int countOfNumbers = 0;
+        countOfNumbers++;
+        asc++;
+        desc++;
+
+
         while (sc.hasNext()) {
-            arr.add(sc.nextInt());
-        }
-
-        while (tt) {
-
-            if (arr.get(i) == arr.get(arr.size() - 1) | arr.get(i + 1) == 0) {
+            neW = sc.nextInt();
+            if (neW == 0) {
                 break;
-            } else {
-                one = arr.get(i);
-                two = arr.get(i + 1);
             }
-            //System.out.println(two + " two ");
+            if (olD == neW) {
+                asc++;
+                desc++;
+            }
 
-            if (one > two) {
-                //System.out.println(one + " 1 cycle one > ");
-                if (one >= two) {
-                    countTwo++;
-                } else {
-                    countTwo--;
-                }
-            } else if (two > one){
-                //System.out.println(two + " 2 cycle two > ");
-                if (two >= one) {
-                    count++;
-                } else {
-                    count--;
-                }
+            if (olD > neW) {
+                asc++;
+            } else if (neW > olD) {
+                desc++;
             }
-            s++;
-            i++;
+            countOfNumbers++;
+            olD = neW;
         }
-        boolean t = s == count | s == countTwo ? true : false;
-        System.out.println(t);
+        System.out.print(countOfNumbers == asc || countOfNumbers == desc);
     }
 }
