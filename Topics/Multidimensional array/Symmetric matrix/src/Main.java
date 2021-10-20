0ef0c24d2c0a5300fbcc33d45f094af18496d0c1
Dnpypy <f300s500h600>
2021-10-20 21:03:17 +0300

    commit

--- Topics/Multidimensional array/Symmetric matrix/src/Main.java
+++ Topics/Multidimensional array/Symmetric matrix/src/Main.java
@@ -5,6 +5,7 @@ class Main {
         // put your code here
         Scanner input = new Scanner(System.in);
         int size = input.nextInt();
+
         int[][] ar = new int[size][size];
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
