a62fa3c05682f10dc9cdfcb75a2092284ee644d5
Dnpypy <denlatypoff@yandex.ru>
2021-10-23 23:50:11 +0300

    4/5 passed

--- Topics/The while and do-while loops/Collatz conjecture/src/Main.java
+++ Topics/The while and do-while loops/Collatz conjecture/src/Main.java
@@ -7,13 +7,15 @@ class Main {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int nums = 1;
+
         if (n % 2 == 0) {
             nums = (n / 2);
             System.out.print(nums);
         } else if (n % 2 != 0) {
-            while (nums != 1);
-            nums = ((n / 2) * 3) + 1;
-            System.out.print(nums + " ");
+            while (nums != 1) {
+                nums = ((n / 2) * 3) + 1;
+                System.out.print(nums + " ");
+            }
         }
 
     }
